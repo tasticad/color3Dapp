0451a7ca219e27b1d242df640af3b021ccb01025
Adrien <tasticad@gmail.com>
2020-07-21 18:36:23 +0200

    Update app

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+
+const DRAG_NOTICE = document.getElementById('js-drag-notice');
+
+
+var loaded = false;
+
+
 var activeOption = 'legs';
 
 
@@ -11,9 +18,159 @@ const colors = [
 	shininess: 60
 },
 {
+	texture: 'img/fabric.jpg',
+	size: [4, 4, 4],
+	shininess: 0
+},
+{
+	texture: 'img/pattern.jpg',
+	size: [8, 8, 8],
+	shininess: 10
+},
+{
 	texture: 'img/denim.jpg',
 	size: [3, 3, 3],
 	shininess: 0
+},
+{
+	texture: 'img/quilt.jpg',
+	size: [6, 6, 6],
+	shininess: 0
+},
+{
+	color: '131417'  
+},
+{
+	color: '374047'  
+},
+{
+	color: '5f6e78'  
+},
+{
+	color: '7f8a93'  
+},
+{
+	color: '97a1a7'  
+},
+{
+	color: 'acb4b9'  
+},
+{
+	color: 'DF9998',
+},
+{
+	color: '7C6862'
+},
+{
+	color: 'A3AB84'
+},
+{
+	color: 'D6CCB1'
+},
+{
+	color: 'F8D5C4'
+},
+{
+	color: 'A3AE99'
+},
+{
+	color: 'EFF2F2'
+},
+{
+	color: 'B0C5C1'
+},
+{
+	color: '8B8C8C'
+},
+{
+	color: '565F59'
+},
+{
+	color: 'CB304A'
+},
+{
+	color: 'FED7C8'
+},
+{
+	color: 'C7BDBD'
+},
+{
+	color: '3DCBBE'
+},
+{
+	color: '264B4F'
+},
+{
+	color: '389389'
+},
+{
+	color: '85BEAE'
+},
+{
+	color: 'F2DABA'
+},
+{
+	color: 'F2A97F'
+},
+{
+	color: 'D85F52'
+},
+{
+	color: 'D92E37'
+},
+{
+	color: 'FC9736'
+},
+{
+	color: 'F7BD69'
+},
+{
+	color: 'A4D09C'
+},
+{
+	color: '4C8A67'
+},
+{
+	color: '25608A'
+},
+{
+	color: '75C8C6'
+},
+{
+	color: 'F5E4B7'
+},
+{
+	color: 'E69041'
+},
+{
+	color: 'E56013'
+},
+{
+	color: '11101D'
+},
+{
+	color: '630609'
+},
+{
+	color: 'C9240E'
+},
+{
+	color: 'EC4B17'
+},
+{
+	color: '281A1C'
+},
+{
+	color: '4F556F'
+},
+{
+	color: '64739B'
+},
+{
+	color: 'CDBAC7'
+},
+{
+	color: '946F43'
 },
 {
 	color: '66533C'
@@ -29,7 +186,7 @@ const colors = [
 },
 {
 	color: '438AAC'
-}  
+}
 ]
 
 
@@ -136,7 +293,7 @@ var dirLight = new THREE.DirectionalLight( 0xffffff, 0.54 );
 	dirLight.position.set( -8, 12, 8 );
 	dirLight.castShadow = true;
 	dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-// Add directional Light to scene    
+// Add directional Light to scene	
 	scene.add( dirLight );
 
 
@@ -176,6 +333,11 @@ function animate() {
 		const canvas = renderer.domElement;
 		camera.aspect = canvas.clientWidth / canvas.clientHeight;
 		camera.updateProjectionMatrix();
+	}
+
+	if (theModel != null && loaded == false) {
+		initialRotation();
+		DRAG_NOTICE.classList.add('start');
 	}
 }
 // the update function:
@@ -281,3 +443,85 @@ function setMaterial(parent, type, mtl) {
 		}
 	});
 }
+
+
+// Function - Opening rotate
+let initRotate = 0;
+
+function initialRotation() {
+	initRotate++;
+
+	if (initRotate <= 120) {
+		theModel.rotation.y += Math.PI / 60;
+	} else {
+		loaded = true;
+	}
+}
+
+
+// Ability to drag swatch panel:
+var slider = document.getElementById('js-tray'), sliderItems = document.getElementById('js-tray-slide'), difference;
+
+function slide(wrapper, items) {
+	var posX1 = 0,
+		posX2 = 0,
+		posInitial,
+		threshold = 20,
+		posFinal,
+		slides = items.getElementsByClassName('tray__swatch');
+  
+	// Mouse events
+	items.onmousedown = dragStart;
+	
+	// Touch events
+	items.addEventListener('touchstart', dragStart);
+	items.addEventListener('touchend', dragEnd);
+	items.addEventListener('touchmove', dragAction);
+
+
+	function dragStart (e) {
+		e = e || window.event;
+		posInitial = items.offsetLeft;
+		difference = sliderItems.offsetWidth - slider.offsetWidth;
+		difference = difference * -1;
+
+		if (e.type == 'touchstart') {
+			posX1 = e.touches[0].clientX;
+		} else {
+			posX1 = e.clientX;
+			document.onmouseup = dragEnd;
+			document.onmousemove = dragAction;
+		}
+	}
+
+	function dragAction (e) {
+		e = e || window.event;
+    
+		if (e.type == 'touchmove') {
+			posX2 = posX1 - e.touches[0].clientX;
+			posX1 = e.touches[0].clientX;
+		} else {
+			posX2 = posX1 - e.clientX;
+			posX1 = e.clientX;
+		}
+
+		if (items.offsetLeft - posX2 <= 0 && items.offsetLeft - posX2 >= difference) {
+			items.style.left = (items.offsetLeft - posX2) + "px";
+		}
+	}
+  
+	function dragEnd (e) {
+		posFinal = items.offsetLeft;
+		if (posFinal - posInitial < -threshold) { } else if (posFinal - posInitial > threshold) {
+
+		} else {
+			items.style.left = (posInitial) + "px";
+		}
+
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+
+}
+
+slide(slider, sliderItems);
